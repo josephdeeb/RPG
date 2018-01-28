4562e530e21bc3edfbc9ea07ecc98eb2eedd81e6
Joseph Besenski <josephdbesenski@gmail.com>
2018-01-28 01:53:20 -0700

    Added IDAble and added search, delete, and add functions to inventory

--- world.py
+++ world.py
@@ -1,4 +1,4 @@
-class Description:
+class Descriptable:
     """
     We need to have the description object represent various descriptions depending upon player status, items, etc.
     """
@@ -11,13 +11,69 @@ class Description:
     def getDesc(self):
         return desc
 
+class IDAble:
+    """
+    This object has a unique ID assigned to it by the game
+    """
+    def __init__(self, ID):
+        self.ID = ID
 
-class Location:
-    def __init__(self, name, desc):
+class Item(IDAble, Descriptable):
+    def __init__(self, ID, name, desc):
+        IDAble.__init__(ID);
         self.name = name
-        self.desc = desc
+        Descriptable.__init__(desc);
+
+class Inventory:
+    def __init__(self, items = []):
+        self.items = items
+
+    def addItem(self, item):
+        self.items.append(item)
+
+    def delItem(self, index = None, ID = None):
+        returned = None
+        if index is not None:
+            returned = self.items.pop(index)
+
+        elif ID is not None:
+            returned = self.items.pop(self.searchItems(ID)[0])
+
+        return returned
+
+    """
+    Searches for item by name or ID, returns a list of tuples where each tuple is:
+    (Item, Index)
+    If searching by name, will return all items matching the name
+    If searching by ID, will return only one item
+    Returns an empty list if item is not found
+    """
+    def searchItems(self, ID = None, name = None):
+        returned = []
+        if ID is not None:
+            for i in range(len(items)):
+                if isinstance(items[i], IDAble):
+                    returned.append((items[i], i))
+                    break
+
+        elif name is not None:
+            for i in range(len(items)):
+                if items[i].name = name:
+                    returned.append((items[i], i))
+
+        if not returned:
+            returned.append((None, None))
+
+        return returned
+
+
+class Location(IDAble, Descriptable):
+    def __init__(self, ID, name, desc):
+        IDAble.__init__(ID)
+        self.name = name
+        Descriptable.__init__(desc)
 
 class World:
-    def __init__(self, name, height = , width) :
+    def __init__(self, name, height = 0, width = 0) :
         self.name = name
-        self.locations = [[]]
+        self.locations = [[0 for x in range(width)] for y in range(height)] #Verify this works
