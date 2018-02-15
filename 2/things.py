616f4ae4eec4877fe1f1809bc3526c0ea0959f0f
joseph.besenski <joseph.besenski@zone18-ed.cs.ucalgary.ca>
2018-02-15 13:49:39 -0700

    Changing how transitions work

--- 2/things.py
+++ 2/things.py
@@ -1,40 +1,65 @@
 class Enum:
     pass
 
+class Transitions:
+    def __init__(self, )
+
 class State:
     def __init__(self, name, transitions, char):
         # String
         self.name = name
-        # Dict where keys are action Strings, values are a 2tuple with (amount, State)
+        # Dict where keys are action Strings, values are a 2tuple with (amount, stateName to go to)
+        # If amount is negative, it means less than or equal to (amount)
         self.transitions = transitions
         # Char
         self.char = char
 
 class Thing:
-    def __init__(self, name, states, currentState):
+    def __init__(self, name, states, currentState, attributes):
         # String
         self.name = name
         # Dictionary
         self.states = states
         # MAKE SURE currentState IS IN states beforehand!!!
         self.currentState = currentState
+        # Dict of attributes this thing has
+        self.attributes = attributes
 
     def isCharable(self):
-        return currentState.char != None
+        return self.currentState.char != None10
 
     def getChar(self):
-        return currentState.char
+        return self.currentState.char
 
-    def update(self, action, thing, amount = None):
+    def getName(self):
+        return self.currentState.name
+
+    def changeState(self, name):
+        self.currentState = states[name]
+
+    def update(self, action, amount = 0):
+        """
         if action in self.currentState.transitions.keys():
             transition = self.currentState.transitions[action]
-            if
+            if amount >= transition[0]:
+                changeState(transition[1])
+        """
 
 
-
-dftransitions = {
-    "DAMAGE": Transition()
+dungeonFloorTransitions = {
+    # if dungeonFloor has attribute named HEALTH, if dungeonFloor is in state NORMAL, if HEALTH is less than or equal to 10, change state to BROKEN
+    "ATTR_HEALTH": {"NORMAL": (-10, "BROKEN"), }
 }
+
+"""
+dungeonFloorNotBrokenTransitions = {
+    "HEALTH": (-10, "BROKEN")
+}
+
+dungeonFloorBrokenTransitions = {
+    "REPAIR": (0, "NOTBROKEN")
+}
+"""
 
 things = [
     Thing("Dungeon Floor", ),
