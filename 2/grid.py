0c38cb0a7d5904192a0dfb84ef5986843a032701
Joseph Besenski <josephdbesenski@gmail.com>
2018-02-22 23:30:24 -0700

    working on grid

--- 2/grid.py
+++ 2/grid.py
@@ -1,4 +1,3 @@
-import things
 
 class Grid:
     def __init__(self, x, y):
@@ -7,10 +6,36 @@ class Grid:
     def setChar(self, x, y, char):
         self.grid[x][y] = char
 
+
 class Map:
     def __init__(self, name, x, y):
         self.name = name
-        self.things = [[list() for i in range(x)] for j in range(y)]
+        self.things = [[[] for i in range(x)] for j in range(y)]
+        self.x = x
+        self.y = y
 
     def addThing(self, thing, x, y):
         self.things[x][y].append(thing)
+
+    def getChar(self, x, y):
+        return self.things[y][x][-1].getAttribute("char")
+
+    def __str__(self):
+        returned = ""
+        for y in range(len(self.things)):
+            for x in range(len(self.things[y])):
+                returned += self.getChar(x, y)
+            returned += "\n"
+        return returned
+
+class Thing:
+    def __init__(self, name, char):
+        self.attributes = {"char": char, "name": name}
+
+    def getAttribute(self, string):
+        return self.attributes[string]
+
+map = Map("fuck", 10, 10)
+map.addThing(Thing("wall", "@"), 0, 0)
+map.addThing(Thing("wall", "@"), 0, 1)
+print(map)
