d27196d5fb9e4437c73fb0d28c7968efc7f234d7
Joseph Besenski <josephdbesenski@gmail.com>
2018-07-27 02:07:27 -0600

    worked on server networking

--- html/server.py
+++ html/server.py
@@ -6,14 +6,39 @@ import time
 USERS = set()
 BEINGS = dict()
 ENTITIES = dict()
+NEW_BULLETS = dict()
+BULLETS = dict()
+
+class Bullet:
+    def __init__(self, x, y, velocityX, velocityY, size):
+        self.x = x
+        self.y = y
+        self.velocityX = velocityX
+        self.velocityY = velocityY
+        self.size = size
+
+    def updatePosition(self):
+
+
+# Sending packets, we need:
+#   Sequence number of packet we are responding to for each client
+#   Newly created entities
+#   For each being that has changed:
+#       If the being has changed in view of the client, then send necessary movements for client to interpolate
+#       Otherwise, ignore that being and don't send any data regarding it
+
+def inputProcessing():
+
 
 def getStateJSON():
-    return json.dumps({'type': 'beings', **BEINGS}, {'type': 'entities', **ENTITIES})
+    return json.dumps({'beings': BEINGS, 'new_bullets': NEW_BULLETS})
+
+async def register(websocket):
+    USERS.add(websocket)
 
 async def inputHandler(websocket, path):
     await register(websocket)
     try:
-        #Send
         await websocket.send(getStateJSON())
         async for message in websocket:
             await inputProcessing(message)
