7785a33b6a98d3c6f83dcc886e695759714064a5
Joseph Besenski <josephdbesenski@gmail.com>
2018-09-25 23:22:18 -0600

    pixi work

---
+++ pxi5/main.js
@@ -0,0 +1,13 @@
+function start() {
+    let app = new PIXI.Application({width: 256, height: 256});
+
+    document.body.appendChild(app.view);
+/*
+    let type = "WebGL"
+    if(!PIXI.utils.isWebGLSupported()){
+        type = "canvas"
+    }
+
+    PIXI.utils.sayHello(type)
+*/
+}
